e0245347e58e83cc6ef8e1b2840a38161ff99010
anthonykao10 <anthonykao10@gmail.com>
2019-09-01 22:26:18 +0000

    Refactor into separate functions

--- fetcher.js
+++ fetcher.js
@@ -1,85 +1,59 @@
 const request = require('request');
 const fs = require('fs');
 const readline = require('readline');
-const [, , ...args] = process.argv;
-const url = args[0];
-const file = args[1];
 
-if (!args.length) return;
-
-fs.open(file, 'wx', (err, fd) => {
-  // IF file exists
-  if (err && err.code === 'EEXIST') {
-    // Ask user to overwrite or not
-    rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-
-    rl.question(`File: '${file}' already exists. Would you like to overwrite? (y or n)\n`, (answer) => {
-      // IF 'no', exit
-      if (answer === 'n') {
-        rl.close(); // unnecessary?
-        process.exit();
-
-      // IF 'yes', send request and write to file
-      } else if (answer === 'y') {
-        try {
-          request(url, (err, res, body) => {
-            // Handle invalid domain
-            if (err && err.code === 'ENOTFOUND') {
-              console.log(`\nThe domain '${url}' is invalid...\n`);
-              return;
-            }
-            // Handle invalid resource path (404)
-            if (res.statusCode === 404) {
-              console.log(`\nThe resource ${url} is invalid...\n`);
-              return;
-            }
-
-            // Write res body to file
-            fs.writeFile(file, body, (err) => {
-              if (err) {
-                console.log(`Error trying to write to ${file}`);
-                return;
-              }
-              console.log(`Downloaded and saved ${body.length} bytes to ./${file}`);
-            });
-          });
-        } catch (err) {
-          console.log(`\nThe url ${url} is invalid...\n`);
-        }
-
-        rl.close();
+const getPage = function(url, file) {
+  try {
+    request(url, (err, res, body) => {
+      // Handle invalid domain
+      if (err && err.code === 'ENOTFOUND') {
+        console.log(`\nThe domain '${url}' is invalid...\n`);
+        return;
       }
-    });
+      // Handle invalid resource path (404)
+      if (res.statusCode === 404) {
+        console.log(`\nThe resource ${url} is invalid...\n`);
+        return;
+      }
 
-  // IF file doesn't exist, send request and write file
-  } else {
-    try {
-      request(url, (err, res, body) => {
-        // Handle invalid domain
-        if (err && err.code === 'ENOTFOUND') {
-          console.log(`\nThe domain '${url}' is invalid...\n`);
+      fs.writeFile(file, body, (err) => {
+        if (err) {
+          console.log(`Error trying to write to ${file}`);
           return;
         }
-        // Handle invalid resource path (404)
-        if (res.statusCode === 404) {
-          console.log(`\nThe resource ${url} is invalid...\n`);
-          return;
-        }
-
-        // Write res body to file
-        fs.writeFile(file, body, (err) => {
-          if (err) {
-            console.log(`Error trying to write to ${file}`);
-            return;
-          }
-          console.log(`Downloaded and saved ${body.length} bytes to ./${file}`);
-        });
+        console.log(`Downloaded and saved ${body.length} bytes to ./${file}`);
       });
-    } catch (err) {
-      console.log(`\nThe url ${url} is invalid...\n`);
-    }
+    });
+  } catch (err) {
+    console.log(`\nThe url ${url} is invalid...\n`);
   }
-});
+}
+
+const fetcher = function() {
+  const [, , url, file] = process.argv;
+  
+  if (!url || !file) return;
+  
+  fs.open(file, 'wx', (err, fd) => {
+    // Check if file exists
+    if (err && err.code === 'EEXIST') {
+      // Prompt user to overwrite or not
+      rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+  
+      rl.question(`File: '${file}' already exists. Would you like to overwrite? (y or n)\n`, (answer) => {
+        if (answer === 'n') process.exit();
+        if (answer === 'y') getPage(url, file);
+        rl.close();
+      });
+  
+    // Create file if doesn't exist
+    } else {
+      getPage(url, file);
+    }
+  });
+}
+
+fetcher();
